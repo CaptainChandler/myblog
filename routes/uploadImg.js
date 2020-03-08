b0895887486ee6ee35a20ffb7b6c11d41d25291f
moch_github <vita_chen@126.com>
2020-03-08 21:25:10 +0800

    IMG存储到服务器
    base64存储图片导致数据流过大，MANGODB存储失败。改回将图片存储在服务器

--- routes/uploadImg.js
+++ routes/uploadImg.js
@@ -2,6 +2,7 @@ var express = require('express');
 var routers = express.Router();
 var formidable = require('formidable');
 var fs =  require('fs');
+var moment = require('moment');
 
 routers.get('/', function(req, res, next){
 
@@ -14,28 +15,30 @@ routers.post('/', function(req, res, next){
   form.uploadDir = dir;
   form.parse(req, function(err, fields, files){
     let oldPath = files.myFile.path;
-    // let picname = files.myFile.name;
+    let picname = files.myFile.name;
+
+    // let picname = moment(new Date()).format('YYYYMMDDHHmmss').toString();
     
-    // let newPath = dir + picname;
+    let newPath = dir + picname;
     // console.log(__dirname);
-    let bitpic = fs.readFileSync(oldPath);
-    let base64pic =  Buffer.from(bitpic, "binary").toString("base64");
+    // let bitpic = fs.readFileSync(oldPath);
+    // let base64pic =  Buffer.from(bitpic, "binary").toString("base64");
     // console.log(base64pic);
 
-    //del temp pic from server
-    let abPath = __dirname.substring(0,13) + oldPath; 
-    fs.unlinkSync(abPath);
+    // //del temp pic from server
+    // let abPath = __dirname.substring(0,13) + oldPath; 
+    // fs.unlinkSync(abPath);
     
-    // fs.rename(oldPath, newPath, function(err){
-    //   if(err){
-    //     res.send({isOK: false, err});
-    //   }
-    //   let resPath = newPath.replace("./public", "http://localhost:3000");
-    //   res.send({isOK: true, url: ["data:image/jpg;base64," + resPath]});
+    fs.rename(oldPath, newPath, function(err){
+      if(err){
+        res.send({isOK: false, err});
+      }
+      let resPath = newPath.replace("./public", "http://localhost:3000");
+      res.send({isOK: true, url: [resPath]});
     
-    // });
+    });
 
-    res.send({isOK: true, url: ["data:image/jpg;base64," + base64pic]});
+    // res.send({isOK: true, url: ["data:image/jpg;base64," + base64pic]});
     
   })
 });
